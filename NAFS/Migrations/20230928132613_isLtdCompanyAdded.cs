88d80e3596560ad6c0f9736773db18a6e6400edd
Muslim92 <muslimabbas@outlook.com>
2023-09-28 14:28:11 +0100

    dto added

---
+++ NAFS/Migrations/20230928132613_isLtdCompanyAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NAFS.Migrations
+{
+    /// <inheritdoc />
+    public partial class isLtdCompanyAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isLtdCompany",
+                table: "AssignServices",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isLtdCompany",
+                table: "AssignServices");
+        }
+    }
+}
